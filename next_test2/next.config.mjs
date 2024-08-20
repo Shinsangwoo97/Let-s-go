8f34bd4c43567bfff8fd9e8cba3c29ff04180a3a
shinsangwoo <ssw2570@naver.com>
2024-08-20 16:38:22 +0900

    reactcomplier 적용 nextjs-15

--- next_test2/next.config.mjs
+++ next_test2/next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// npm install babel-plugin-react-compiler 설치
 
-export default nextConfig;
+// reactcomplier 적용 nextjs-15
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+      reactCompiler: true,
+    },
+  }
+   
+  module.exports = nextConfig
